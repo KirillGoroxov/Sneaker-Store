34270efecaaa4ff428eac4b7e5c9cf135d5cbb7c
KirillGoroxov <106907758+KirillGoroxov@users.noreply.github.com>
2023-05-21 13:37:03 +0400

    test

--- src/сomponets/Header/Header.tsx
+++ src/сomponets/Header/Header.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
 import Basket from './Basket/Basket'
 import Navbar from './Navbar/Navbar'
+import c from './Header.module.scss'
 const Header: React.FC = () => {
   const [basket, setBasket] = useState<boolean>(false)
   const changeBasket = () => {
     setBasket(!basket)
   }
+  document.addEventListener('scroll', () => {
+    if (window.scrollY > 30) {
+      (document.getElementById('header') as HTMLHeadElement).style.backgroundColor = '#d6d6d6'
+    }
+    if (window.scrollY < 30) {
+      (document.getElementById('header') as HTMLHeadElement).style.backgroundColor = ''
+    }
+  })
   return (
-    <header>
+    <header className={c.wrapper} id='header'>
       <Navbar changeBasket={changeBasket} />
       <Basket
         basket={basket}
