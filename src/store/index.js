810c09305637b0ed25bab2261450005698617eef
KirillGoroxov <106907758+KirillGoroxov@users.noreply.github.com>
2023-06-01 11:09:52 +0400

    test

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import products from './products'
-import basket from './basket'
-export default configureStore({
-  reducer: {
-    products: products,
-    basket: basket
-  }
-})
